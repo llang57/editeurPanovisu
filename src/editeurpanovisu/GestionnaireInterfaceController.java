ba4dd6a67f97264194000472d20607b652e1020c
llang57 <github@lemondea360.fr>
2014-06-04 23:52:58 +0200

    Création du gestionnaire d'interface

---
+++ src/editeurpanovisu/GestionnaireInterfaceController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package editeurpanovisu;
+
+import javafx.scene.control.Tab;
+
+/**
+ *
+ * @author LANG Laurent
+ */
+public class GestionnaireInterfaceController {
+    private static Tab tabInterface=new Tab();
+    
+    public Tab creeInterface(){
+        
+        return tabInterface;
+    }
+    
+}
