051b78a9ba6c803132bb6496c9de7f9e68a2788d
llang57 <llang57@users.noreply.github.com>
2025-11-03 17:32:28 +0000

    docs: 📖 Mise à jour Javadoc 39c489255a08d838dedacf8bce259f2a0e414225

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"editeurpanovisu"},{"l":"editeurpanovisu.gpu"},{"l":"editeurpanovisu.util"},{"l":"test"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"editeurpanovisu"},{"l":"editeurpanovisu.config"},{"l":"editeurpanovisu.gpu"},{"l":"editeurpanovisu.util"},{"l":"test"}];updateSearchResults();
